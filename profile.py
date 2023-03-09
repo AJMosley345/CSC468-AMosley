9653f78ac3622ff0460714a055770cf690c6eefa
AJ Mosley <ajmosley345@gmail.com>
2023-03-09 08:50:28 -0500

    Change Dockerfile

--- profile.py
+++ profile.py
@@ -5,21 +5,9 @@ import geni.rspec.igext as IG
 pc = portal.Context()
 request = pc.makeRequestRSpec()
 
-tourDescription = \
-"""
-This profile provides the template for a compute node with Docker installed on Ubuntu 18.04
-"""
-
-#
-# Setup the Tour info with the above description and instructions.
-#  
-tour = IG.Tour()
-tour.Description(IG.Tour.TEXT,tourDescription)
-request.addTour(tour)
-
 node = request.XenVM("docker")
-node.cores = 8
-node.ram = 8192
+node.cores = 4
+node.ram = 4096
 node.routable_control_ip = "true" 
 
 bs_landing = node.Blockstore("bs_image", "/image")
@@ -28,5 +16,6 @@ bs_landing.size = "500GB"
 node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU20-64-STD"
 node.routable_control_ip = "true"
 node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/install_docker.sh"))
-  
+node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/"))
+
 pc.printRequestRSpec(request)
